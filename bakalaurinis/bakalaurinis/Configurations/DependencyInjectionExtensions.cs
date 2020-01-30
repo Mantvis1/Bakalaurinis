f870870378fefe70aaae57e31d14a4d00fc08ae8
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2020-01-30 19:57:21 +0200

    Initial commit

---
+++ bakalaurinis/bakalaurinis/Configurations/DependencyInjectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace bakalaurinis.Configurations
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddAllDependencies(this IServiceCollection service)
+        {
+            return service
+                .AddInfrastructureDependencies()
+                .AddApplicationDependencies();
+        }
+
+        public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection service)
+        {
+            return null;
+        }
+
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection service)
+        {
+            return null;
+        }
+    }
+}
---
+++ bakalaurinis/bakalaurinis/Configurations/StartupExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace bakalaurinis.Configurations
+{
+    public static class StartupExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("holidays", new OpenApiInfo { Title = "Bakalaurinis", Version = "v1" });
+            });
+
+            return services;
+        }
+
+        public static void ConfigureAndUseSwagger(this IApplicationBuilder app)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/holidays/swagger.json", "??");
+                options.RoutePrefix = "swagger";
+            });
+        }
+
+    }
+}
