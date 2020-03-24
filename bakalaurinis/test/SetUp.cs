e2fc709f27318afc00dcd098d6e09d752ffaa702
Mantvydas Razulevicius <razuleviciusmantvydas@gmail.com>
2020-03-24 13:32:12 +0200

    Set up tests

---
+++ bakalaurinis/test/SetUp.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using bakalaurinis.Configurations;
+using bakalaurinis.Infrastructure.Database;
+using bakalaurinis.Infrastructure.Database.Models;
+using bakalaurinis.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace test
+{
+    class SetUp
+    {
+        private TimeService _timeService = new TimeService();
+        private Work[] _works;
+        private User[] _users;
+        private Invitation[] _invitations;
+        private UserSettings[] _userSettings;
+        private Message[] _messages;
+        private MessageTemplate[] _messageTemplates;
+
+        private DatabaseContext _context;
+        public DatabaseContext DatabaseContext =>
+            _context ??
+            throw new InvalidOperationException("Run initialize method before accessing this property.");
+
+        private IMapper _mapper;
+        public IMapper Mapper =>
+            _mapper ??
+            throw new InvalidOperationException("Run initialize method before accessing this property.");
+
+        public void Initialize()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: "Test_Database")
+                .UseInternalServiceProvider(serviceProvider)
+                .Options;
+
+            var configuration = GetConfiguration();
+            _context = new DatabaseContext(options);
+            Seed(_context);
+
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperConfiguration());
+            });
+            _mapper = config.CreateMapper();
+        }
+
+        public Microsoft.Extensions.Configuration.IConfiguration GetConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                          .SetBasePath(Directory.GetCurrentDirectory())
+                          .Build();
+            return config;
+        }
+
+        private async void Seed(DatabaseContext context)
+        {
+            var config = GetConfiguration();
+
+            _users = new[] {
+            new User
+            {
+                Id = 1,
+                Username = "test1",
+                Password = "testPassword"
+            },
+            new User
+            {
+                Id = 2,
+                Username = "test2",
+                Password = "testPassword2"
+            }
+            };
+
+            context.Users.AddRange(_users);
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ bakalaurinis/test/Tests/UserTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using bakalaurinis.Configurations;
+using bakalaurinis.Infrastructure.Database;
+using bakalaurinis.Infrastructure.Repositories;
+using bakalaurinis.Services;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace test.Tests
+{
+    public class UserTests
+    {
+        private readonly DatabaseContext _context;
+        private readonly UserService _userService;
+        private readonly IMapper _mapper;
+
+
+        public UserTests()
+        {
+            var setUp = new SetUp();
+            setUp.Initialize();
+            _context = setUp.DatabaseContext;
+
+            var _mapper = setUp.Mapper;
+            var userRepository = new UsersRepository(_context);
+            var appSettingsMock = new Mock<IOptions<AppSettings>>().Object;
+            var userSettingsRepository = new UserSettingsRepository(_context);
+            var invitationsRepository = new InvitationRepository(_context);
+
+            var userSettingsService = new UserSettingsService(_mapper, userSettingsRepository);
+
+            _userService = new UserService(appSettingsMock, _mapper, userRepository, userSettingsService, invitationsRepository);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void GetUserNameDtoById(int id)
+        {
+            var userNameDto = await _userService.GetNameById(id);
+
+            Assert.NotNull(userNameDto);
+        }
+    }
+}
